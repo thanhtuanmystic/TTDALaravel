4de9f69598c2d783dac566e848bfde95f80b7fff
Thanh Tuan <tuanvp2001@gmail.com>
2023-10-04 17:18:11 +0700

    add product(đang có 1 lỗi nhỏ, fix sau)

--- resources/views/admin/allproducts.blade.php
+++ resources/views/admin/allproducts.blade.php
@@ -5,6 +5,11 @@
 @section('content')
     <div class="container-xxl flex-grow-1 container-p-y">
         <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Page/</span> All Products</h4>
+        @if (session()->has('message'))
+            <div class="alert alert-success">
+                {{ session()->get('message') }}
+            </div>
+        @endif
         <div class="card">
             <h5 class="card-header">Available All Product Information</h5>
             <div class="table-responsive text-nowrap">
